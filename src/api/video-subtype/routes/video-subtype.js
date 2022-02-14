500abc2d92a29302769751c0f5cc216b44b16d9e
DrMint <thomas.barillot@etu.u-bordeaux.fr>
2022-02-14 06:16:52 +0100

    Added subtype for video and more langui

---
+++ src/api/video-subtype/routes/video-subtype.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * video-subtype router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::video-subtype.video-subtype');
---
+++ src/api/video-subtype/services/video-subtype.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * video-subtype service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::video-subtype.video-subtype');
