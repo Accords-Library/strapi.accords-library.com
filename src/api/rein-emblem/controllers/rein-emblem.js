b2d48af93370426379454b9f4e29d93502030eb6
DrMint <29893320+DrMint@users.noreply.github.com>
2023-02-18 11:54:29 +0100

    Added rein-emblems

---
+++ src/api/rein-emblem/controllers/rein-emblem.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rein-emblem controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::rein-emblem.rein-emblem');
---
+++ src/api/rein-emblem/services/rein-emblem.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rein-emblem service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::rein-emblem.rein-emblem');
