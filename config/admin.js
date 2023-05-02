cd0cc4c995ad2cdced49f14cb57c785780cc5c50
DrMint <29893320+DrMint@users.noreply.github.com>
2023-05-02 11:37:53 +0200

    Updated deps

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,9 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '5b7570ef6a700c64483c58ca224d87f7'),
   },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT', '2Aw/HRoKBtolbCYat2f2GA=='),
+    },
+  },
 });
